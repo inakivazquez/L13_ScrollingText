dad1c391ace6e9bb97d4947c421ee27c846d4dd0
Juan Ignacio Vazquez Gomez <ivazquez@deusto.es>
2019-02-02 19:09:39 +0100

    Initial commit

---
+++ app/src/main/java/es/deusto/l13_scrollingtext/MainActivity.java
@@ -0,0 +1,13 @@
+package es.deusto.l13_scrollingtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
